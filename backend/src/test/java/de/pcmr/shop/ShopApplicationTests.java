275b2feb92007755aa36570ecfcebce8297be722
Fynn Lohse <mail@fynn-lohse.de>
2020-06-27 15:29:59 +0200

    Add backend

---
+++ backend/src/test/java/de/pcmr/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package de.pcmr.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
