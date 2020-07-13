8592e55487242f01466f2339978e28e6015219fc
Jonas Pfannkuche <jonas.pfannkuche@hdnet.de>
2020-07-13 17:09:46 +0200

    Insert template structure of stage slider

--- frontend/components/home/stage.vue
+++ frontend/components/home/stage.vue
@@ -1,11 +1,45 @@
 <template>
-    <h1>Stage</h1>
+    <div class="stage my-5">
+        <div class="row">
+            <div class="col-4">
+                <div class="text">
+                    <h2 class="text-primary">Erlebe die Schlacht wie nie zuvor</h2>
+                    <p>
+                        Der neuste 144Hz Monitor von Acer bietet ein unfassbares HDR-600 Farbspektrum! Darüber hinaus wird
+                        mit der 4K Auflösung jedes Detail sichtbar.
+                    </p>
+                </div>
+                <b-button variant="primary" class="w-100">
+                    Zum Produkt
+                </b-button>
+            </div>
+            <div class="col-8">
+                <div class="outer-box">
+                    <div class="box ml-auto">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {
-    name: 'HomeStage',
-}
+    export default {
+        name: 'HomeStage',
+    }
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+    .outer-box {
+        overflow: hidden;
+    }
+
+    .box {
+        background-color: $primary;
+        height: 15rem;
+        width: 30rem;
+        border-radius: 0.75rem 0 0 5.625rem / 0.75rem 0 0 5rem;
+        transform: skew(10deg) translateX(30px);
+    }
+</style>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,14 +1,17 @@
 <template>
     <b-container>
+        <home-stage />
         <home-article-overview />
     </b-container>
 </template>
 
 <script>
 import HomeArticleOverview from '~/components/home/articleOverview'
+import HomeStage from '~/components/home/stage'
 
 export default {
     components: {
+        HomeStage,
         HomeArticleOverview,
     },
 }
