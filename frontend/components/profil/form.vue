0b01954823f5deb77c9d640a30cc22d4e0840d3a
Jonas Pfannkuche <jonas.pfannkuche@hdnet.de>
2020-07-23 18:23:14 +0200

    Insert profil page

---
+++ frontend/components/profil/form.vue
@@ -0,0 +1,37 @@
+<template>
+    <b-form>
+        <div class="mb-4" role="group">
+            <label for="firstname">
+                Vorname
+            </label>
+            <b-form-input id="firstname" disabled :value="user.firstName" />
+        </div>
+
+        <div class="mb-4" role="group">
+            <label for="lastname">
+                Nachname
+            </label>
+            <b-form-input id="lastname" disabled :value="user.lastName" />
+        </div>
+
+        <div class="mb-4" role="group">
+            <label for="email">
+                E-Mail
+            </label>
+            <b-form-input id="email" disabled :value="user.email" />
+        </div>
+    </b-form>
+</template>
+
+<script>
+export default {
+    name: 'ProfilForm',
+    data() {
+        return {
+            user: this.$auth.user,
+        }
+    },
+}
+</script>
+
+<style scoped></style>
--- frontend/pages/profil.vue
+++ frontend/pages/profil.vue
@@ -1,10 +1,17 @@
 <template>
-    <h1>Profil</h1>
+    <b-container class="d-flex justify-content-center my-5">
+        <div class="col-12 col-sm-10 col-md-6 col-xl-5">
+            <profil-form />
+        </div>
+    </b-container>
 </template>
 
 <script>
+import ProfilForm from '~/components/profil/form'
+
 export default {
     name: 'Profil',
+    components: { ProfilForm },
     middleware: ['auth'],
 }
 </script>
