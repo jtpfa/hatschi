3136eea9eb89f6e432bb89248858178eb810a60d
Fynn Lohse <mail@fynn-lohse.de>
2020-07-24 23:54:07 +0200

    prepared serving images

--- backend/src/main/java/de/pcmr/shop/config/WebConfig.java
+++ backend/src/main/java/de/pcmr/shop/config/WebConfig.java
@@ -3,14 +3,24 @@ package de.pcmr.shop.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
+    @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOrigins("*");
+        registry.addMapping("/api/**")
+                .allowedOrigins("*");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/media/**")
+                .addResourceLocations("file:./media/");
     }
 }
