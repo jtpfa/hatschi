257a2cef1bc45286cc239fc76f5aedd520099bc5
Fynn Lohse <mail@fynn-lohse.de>
2020-07-24 18:14:53 +0200

    add validation constraints

--- backend/src/main/java/de/pcmr/shop/domain/ArticleEntity.java
+++ backend/src/main/java/de/pcmr/shop/domain/ArticleEntity.java
@@ -1,9 +1,12 @@
 package de.pcmr.shop.domain;
 
+import org.hibernate.validator.constraints.Range;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Lob;
 import javax.persistence.Table;
+import javax.validation.constraints.Size;
 
 import static de.pcmr.shop.domain.AbstractEntity.TABLE_PREFIX;
 
@@ -12,23 +15,28 @@ import static de.pcmr.shop.domain.AbstractEntity.TABLE_PREFIX;
 public class ArticleEntity extends AbstractEntity {
 
     @Column(nullable = false)
+    @Size(min = 4, max = 255, message = "Der Name muss zwischen {min} und {max} Zeichen lang sein")
     private String name;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 4096)
+    @Size(min = 4, max = 4096, message = "Die Kurzbeschreibung muss zwischen {min} und {max} Zeichen lang sein")
     @Lob
     private String description;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 32768)
+    @Size(min = 4, max = 32768, message = "Die Details müssen zwischen {min} und {max} Zeichen lang sein")
     @Lob
     private String details;
 
     @Column(nullable = false)
+    @Range(min = 0, max = 99999999, message = "Der Preis muss zwischen {min} und {max} Cent liegen")
     private int price;
 
     @Column(nullable = false)
     private ArticleStatusEnum status;
 
     @Column(nullable = false)
+    @Range(min = 0, max = 999999, message = "Der Lagerbestand muss zwischen {min} und {max} liegen")
     private int stock;
 
     public String getName() {
