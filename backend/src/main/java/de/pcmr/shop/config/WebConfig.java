faa7d61d793650e3fb2279942bfa5ee52a7a69d7
Fynn Lohse <mail@fynn-lohse.de>
2020-08-06 18:38:35 +0200

    remove unused import

--- backend/src/main/java/de/pcmr/shop/config/WebConfig.java
+++ backend/src/main/java/de/pcmr/shop/config/WebConfig.java
@@ -1,6 +1,5 @@
 package de.pcmr.shop.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
