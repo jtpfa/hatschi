972b68fffff712bb2cef20e631ec45cc29e3cf67
Jonas Pfannkuche <jonas.pfannkuche@hdnet.de>
2020-08-17 01:08:53 +0200

    Move stage image to assets folder

--- frontend/components/home/slide/facts.vue
+++ frontend/components/home/slide/facts.vue
@@ -3,7 +3,7 @@
         <home-slide-background-shape>
             <div class="col-12 col-sm-5 mt-2 mt-sm-0">
                 <div class="lazy-image d-flex justify-content-center align-items-center">
-                    <b-img-lazy src="/img/stage-sample.png" />
+                    <b-img-lazy src="~assets/img/stage-sample.png" />
                     <spinner color="white" :size="6" />
                 </div>
             </div>
