242895f769171a9fea28e435e71fc4f513cd049b
Jonas Pfannkuche <jonas.pfannkuche@hdnet.de>
2020-08-25 15:01:00 +0200

    Insert card for orders

--- frontend/pages/bestellungen.vue
+++ frontend/pages/bestellungen.vue
@@ -1,10 +1,23 @@
 <template>
-    <h1>Bestellungen</h1>
+    <b-container class="my-5">
+        <h1>Bestellungen</h1>
+
+        <customer-related-orders />
+    </b-container>
 </template>
 
 <script>
+import CustomerRelatedOrders from '~/components/shop/account/orders'
+
 export default {
     name: 'Bestellungen',
+    components: { CustomerRelatedOrders },
+    middleware: ['auth'],
+    head() {
+        return {
+            title: `${this.$route.name.replace(/^\w/, c => c.toUpperCase())} – PC Masterrace`,
+        }
+    },
 }
 </script>
 
--- frontend/pages/profil.vue
+++ frontend/pages/profil.vue
@@ -2,21 +2,16 @@
     <b-container class="d-flex justify-content-center my-5">
         <div class="col-12 col-sm-10 col-md-6 col-xl-5">
             <profil-form />
-
-            <h2>Bestellungen</h2>
-
-            <all-orders />
         </div>
     </b-container>
 </template>
 
 <script>
-import AllOrders from '~/components/shop/order'
 import ProfilForm from '~/components/shop/profil/form'
 
 export default {
     name: 'Profil',
-    components: { AllOrders, ProfilForm },
+    components: { ProfilForm },
     middleware: ['auth'],
     head() {
         return {
