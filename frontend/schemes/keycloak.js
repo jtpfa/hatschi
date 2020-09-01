acaf8e3d4fd9f6dd8baf719c1b9adca5077719ff
Jonas Pfannkuche <jonas.pfannkuche@hdnet.de>
2020-09-01 20:27:40 +0200

    Fix invalid session bug

--- frontend/schemes/keycloak.js
+++ frontend/schemes/keycloak.js
@@ -102,7 +102,23 @@ export default class KeyCloakScheme {
         // Ditch any leftover local tokens before attempting to log in
         await this.$auth.reset()
 
-        const { response, result } = await this.$auth.request(xhrData, this.options.endpoints.login, true)
+        const { response, result } = await this.$auth
+            .request(xhrData, this.options.endpoints.login, true)
+            .catch(error => {
+                if (error.response) {
+                    // The request was made and the server responded with a status code
+                    // that falls out of the range of 2xx
+                    throw Error(error.response.data.error_description)
+                } else if (error.request) {
+                    // The request was made but no response was received
+                    // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                    // http.ClientRequest in node.js
+                    throw Error(error.request)
+                } else {
+                    // Something happened in setting up the request that triggered an Error
+                    throw Error(error.message)
+                }
+            })
 
         this.setRoles(result)
 
